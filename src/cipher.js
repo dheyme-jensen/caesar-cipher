c11b41a5f5c4a0bcfc6bcea9c0e163efdcf9e00f
dheyme-jensen <dheymejensen@hotmail.com>
2019-01-31 17:59:46 -0200

    primeiro commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,61 @@
-window.cipher = {
-  // ... 
-};
+function valores(){
+  let msg1 = document.getElementById("msg1").value;
+  let deslocamento = parseInt(document.getElementById("deslocamento").value);
+  cipherEncrypt(msg1, deslocamento);
+ }
+ 
+ function cipherEncrypt (msg1, deslocamento){
+  let arrNumbers = [];
+  let encryptMessage = "";
+ 
+   for (let i = 0; i < msg1.length; i++){
+    arrNumbers.push(msg1[i].charCodeAt())
+   
+    if (arrNumbers[i] >= 65 && arrNumbers[i] <= 90) { 
+       let calculate = ((arrNumbers[i] - 65 + (deslocamento % 26) + 26) % 26) + 65;
+       encryptMessage = encryptMessage + String.fromCharCode(calculate);
+     }
+   
+ 
+    else if (arrNumbers[i] >= 97 && arrNumbers[i] <= 122) {
+       let calculate2 = ((arrNumbers[i] - 97 + (deslocamento % 26) + 26) % 26) + 97;
+       encryptMessage = encryptMessage + String.fromCharCode(calculate2);
+     }
+ 
+    else {encryptMessage = encryptMessage + String.fromCharCode(arrNumbers[i])
+     } 
+   }  let msg2 = document.getElementById("encryptResult").innerHTML= (encryptMessage);
+     return (encryptMessage);  
+      
+ }
+ 
+ function valores2(){
+  msg1 = document.getElementById("msg1").value;
+  deslocamento = parseInt(document.getElementById("deslocamento").value);
+  cipherDecrypt(msg1, deslocamento);
+ }
+ 
+  function cipherDecrypt (msg1, deslocamento){
+  let array = [];
+  let decryptMessage = "";
+ 
+   for (let t = 0; t < msg1.length; t++){
+    array.push(msg1[t].charCodeAt())
+   
+    if (array[t] >= 65 && array[t] <= 90) { 
+       let conta = ((array[t] - 65 - (deslocamento % 26) + 26) % 26) + 65;
+       decryptMessage = decryptMessage + String.fromCharCode(conta);
+     }
+   
+ 
+    else if (array[t] >= 97 && array[t] <= 122) {
+       let conta2 = ((array[t] - 97 - (deslocamento % 26) + 26) % 26) + 97;
+       decryptMessage = decryptMessage + String.fromCharCode(conta2);
+     }
+ 
+    else {decryptMessage = decryptMessage + String.fromCharCode(array[t])
+     }
+   } 
+    let msg2 = document.getElementById("decryptResult").innerHTML= (decryptMessage);
+    return decryptMessage;
+  }
---
+++ src/tests.js
@@ -0,0 +1,38 @@
+function testCoding (expectedResult, obtainedResult){
+    if(obtainedResult === expectedResult){
+      console.log('sucesso');
+    }else{
+      console.log('não deu certo, '  +  'o resultado esperado era '  +  expectedResult +  ' ,mas resultado obtido foi '  + obtainedResult);
+   }
+  }
+
+  testCoding('uftuboep', cipherEncrypt('testando', 1));
+  testCoding('sdrszmcn', cipherEncrypt('testando', 25 ));
+  testCoding('UFTUBOEP', cipherEncrypt('TESTANDO', 27 ));
+  testCoding('paopwjzk', cipherEncrypt('testando', 100 ));
+  testCoding('sdrszmcn', cipherEncrypt('testando', -1));
+  testCoding('UftUboep', cipherEncrypt('TesTando', 1));
+  testCoding('wbnpt uftubs', cipherEncrypt('vamos testar', 1));
+  testCoding('uftuboep123', cipherEncrypt('testando123', 1));
+  testCoding('uêtuáoeõ', cipherEncrypt('têstándõ', 1));
+  testCoding('!uftuboep!@#$', cipherEncrypt('!testando!@#$', 1));
+
+
+ function testDecoding (expectedResult, obtainedResult){
+    if(obtainedResult === expectedResult){
+      console.log('sucesso');
+    }else{
+      console.log('não deu certo, '  +  'o resultado esperado era '  +  expectedResult +  ' ,mas resultado obtido foi '  + obtainedResult);
+   }
+  }
+
+  testDecoding('testando', cipherDecrypt('uftuboep', 1));
+  testDecoding('testando', cipherDecrypt('sdrszmcn', 25 ));
+  testDecoding('TESTANDO', cipherDecrypt('UFTUBOEP', 27 ));
+  testDecoding('testando', cipherDecrypt('paopwjzk', 100 ));
+  testDecoding('testando', cipherDecrypt('sdrszmcn', -1));
+  testDecoding('TesTando', cipherDecrypt('UftUboep', 1));
+  testDecoding('vamos testar', cipherDecrypt('wbnpt uftubs', 1));
+  testDecoding('testando123', cipherDecrypt('uftuboep123', 1));
+  testDecoding('têstándõ', cipherDecrypt('uêtuáoeõ', 1));
+  testDecoding('!testando!@#$', cipherDecrypt('!uftuboep!@#$', 1));
